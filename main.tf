a962064db333d305adf2413340238cdc00624d32
nirajan32 <91690238+nirajan32@users.noreply.github.com>
2022-10-04 02:04:50 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,15 +13,6 @@ resource "aws_s3_bucket" "terraform_state_s3" {
   versioning {
          enabled = true
         }
-
-# Enable server-side encryption by default
-server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 }
 variable "action" {
   default = "apply"
