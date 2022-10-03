5d0470c798678c6756bb099e2ace6c7b71dc1c6a
nirajan32 <91690238+nirajan32@users.noreply.github.com>
2022-10-04 02:09:26 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 #1 -this will create a S3 bucket in AWS
 resource "aws_s3_bucket" "terraform_state_s3" {
   #make sure you give unique bucket name
-  bucket = "terraform-teknobucket1-state-nire" 
+  bucket = "terraform-teknobucket1-state-nire1" 
   force_destroy = true
 # Enable versioning to see full revision history of our state files
   versioning {
